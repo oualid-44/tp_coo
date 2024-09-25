e1da4e6b5d8aa524041205ee8fa24ee6d472d72d
brl4010a <brl4010a@u2-212-06d.univ-tlse3.fr>
2024-09-25 16:38:50 +0200

    add high_level models and admin

--- crayon/high_level/admin.py
+++ crayon/high_level/admin.py
@@ -1,1 +1,15 @@
 # Register your models here.
+from django.contrib import admin
+from . import models
+
+admin.site.register(models.Ville)
+# admin.site.register(models.Local)
+# admin.site.register(models.Objet)
+admin.site.register(models.Machine)
+admin.site.register(models.Usine)
+admin.site.register(models.Ressource)
+admin.site.register(models.SiegeSocial)
+admin.site.register(models.QuantiteRessource)
+admin.site.register(models.Etape)
+admin.site.register(models.Produit)
+admin.site.register(models.Stock)
