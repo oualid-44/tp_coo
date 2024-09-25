8241bc9dac683918f7a494e4b0bd0d7d13b4ca46
brl4010a <brl4010a@u2-212-06d.univ-tlse3.fr>
2024-09-25 16:23:35 +0200

    add high_lelev models and admin

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -1,1 +1,69 @@
+from django.db import models
+
+
 # Create your models here.
+class Ville(models.Model):
+    nom = models.CharField(max_length=100)
+    code_postal = models.IntegerField()
+    prix_m2 = models.IntegerField()
+
+
+class Local(models.Model):
+    ville = models.ForeignKey(Ville, on_delete=models.PROTECT)
+    nom = models.CharField(max_length=100)
+    surface = models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+
+class Objet(models.Model):
+    nom = models.CharField(max_length=100)
+    prix = models.IntegerField(default=0)
+
+    class Meta:
+        abstract = True
+
+
+class Machine(models.Model):
+    nom = models.CharField(max_length=100)
+    prix = models.IntegerField()
+    n_serie = models.IntegerField()
+
+
+class Usine(Local):
+    machines = models.ManyToManyField(Machine)
+
+
+class Ressource(Objet):
+    def __init__(self):
+        pass
+
+
+class SiegeSocial(Local):
+    def __init__(self):
+        pass
+
+
+class QuantiteRessource(models.Model):
+    ressource = models.ForeignKey(Ressource, on_delete=models.PROTECT)
+    quantite = models.IntegerField()
+
+
+class Etape(models.Model):
+    nom = models.CharField(max_length=100)
+    machine = models.ForeignKey(Machine, on_delete=models.PROTECT)
+    quantite_ressource = models.ForeignKey(QuantiteRessource, on_delete=models.PROTECT)
+    duree = models.IntegerField()
+    etape_suivante = models.ForeignKey(
+        "self", null=True, blank=True, on_delete=models.SET_NULL
+    )
+
+
+class Produit(Objet):
+    premiere_etape = models.ForeignKey(Etape, on_delete=models.PROTECT)
+
+
+class Stock(models.Model):
+    objet = models.ForeignKey(Ressource, on_delete=models.PROTECT)
+    nombre = models.IntegerField()
