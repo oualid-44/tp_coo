e72be92b7e1cd47dbd0f4908ee1472fd22c02fb3
brl4010a <brl4010a@u2-212-06d.univ-tlse3.fr>
2024-09-25 17:04:04 +0200

    Ajout des methodes __str__(self)

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -7,11 +7,17 @@ class Ville(models.Model):
     code_postal = models.IntegerField()
     prix_m2 = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.nom} {self.code_postal}"
+
 
 class Local(models.Model):
     ville = models.ForeignKey(Ville, on_delete=models.PROTECT)
     nom = models.CharField(max_length=100)
     surface = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nom} {self.ville} {self.surface} "
 
     class Meta:
         abstract = True
@@ -20,6 +26,9 @@ class Local(models.Model):
 class Objet(models.Model):
     nom = models.CharField(max_length=100)
     prix = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.nom} {self.prix} "
 
     class Meta:
         abstract = True
@@ -30,9 +39,15 @@ class Machine(models.Model):
     prix = models.IntegerField()
     n_serie = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.nom} {self.prix} {self.n_serie}"
+
 
 class Usine(Local):
     machines = models.ManyToManyField(Machine)
+
+    def __str__(self):
+        return f"{self.machines}"
 
 
 class Ressource(Objet):
@@ -46,24 +61,36 @@ class SiegeSocial(Local):
 
 
 class QuantiteRessource(models.Model):
-    ressource = models.ForeignKey(Ressource, on_delete=models.PROTECT)
+    ressource = models.ForeignKey(Ressource, on_delete=models.CASCADE)
     quantite = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.ressource} {self.quantite} "
 
 
 class Etape(models.Model):
     nom = models.CharField(max_length=100)
-    machine = models.ForeignKey(Machine, on_delete=models.PROTECT)
-    quantite_ressource = models.ForeignKey(QuantiteRessource, on_delete=models.PROTECT)
+    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
+    quantite_ressource = models.ForeignKey(QuantiteRessource, on_delete=models.CASCADE)
     duree = models.IntegerField()
     etape_suivante = models.ForeignKey(
-        "self", null=True, blank=True, on_delete=models.SET_NULL
+        "self", null=True, blank=True, on_delete=models.CASCADE
     )
+
+    def __str__(self):
+        return f"{self.nom} {self.machine} {self.quantite_ressource} {self.duree} {self.etape_suivante}"
 
 
 class Produit(Objet):
     premiere_etape = models.ForeignKey(Etape, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return f"{self.premiere_etape}"
+
 
 class Stock(models.Model):
     objet = models.ForeignKey(Ressource, on_delete=models.PROTECT)
     nombre = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.objet} {self.nombre} "
