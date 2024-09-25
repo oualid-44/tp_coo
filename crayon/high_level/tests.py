51875390866ce2adaaacf06165fc74d85fa0e9cd
Oualid <o.aberbour@outlook.com>
2024-09-25 21:34:45 +0200

    Add a test file

--- crayon/high_level/tests.py
+++ crayon/high_level/tests.py
@@ -1,1 +1,10 @@
 # Create your tests here.
+
+from django.test import TestCase
+from .models import Machine
+
+class MachineModelTests(TestCase):
+    def test_machine_creation(self):
+        self.assertEqual(Machine.objects.cout(), 0)
+        Machine.objects.create(nom="TapiS", prix=500, n_serie="10023")
+        self.assertEqual(Machine.objects.cout(), 1)
