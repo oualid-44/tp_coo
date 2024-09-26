2b2b92488f38227e710a30126fca1a9ad3857285
Oualid <o.aberbour@outlook.com>
2024-09-26 10:08:52 +0200

    Rearrage the models file

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -17,7 +17,7 @@ class Local(models.Model):
     surface = models.IntegerField()
 
     def __str__(self):
-        return f"{self.nom} {self.ville} {self.surface} "
+        return f"{self.nom} {self.ville} {self.surface}"
 
     class Meta:
         abstract = True
@@ -28,7 +28,7 @@ class Objet(models.Model):
     prix = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"{self.nom} {self.prix} "
+        return f"{self.nom} {self.prix}"
 
     class Meta:
         abstract = True
@@ -45,10 +45,16 @@ class Machine(models.Model):
 
 class Usine(Local):
     machines = models.ManyToManyField(Machine)
+    # stocks = models.ManyToManyField('Stock', blank=True)
 
     def __str__(self):
-        return f"{self.machines}"
-
+        return ", ".join(machine.nom for machine in self.machines.all())
+        # return f"{self.nom} - Usine"
+    
+    def costs(self):
+        somme_machine = sum(machine.prix for machine in self.machines.all())
+        usine_cost = self.ville.prix_m2 * self.surface
+        return usine_cost + somme_machine
 
 class Ressource(Objet):
     pass
@@ -63,7 +69,10 @@ class QuantiteRessource(models.Model):
     quantite = models.IntegerField()
 
     def __str__(self):
-        return f"{self.ressource} {self.quantite} "
+        return f"{self.ressource} {self.quantite}"
+
+    def costs(self):
+        return self.ressource.prix * self.quantite
 
 
 class Etape(models.Model):
@@ -71,9 +80,7 @@ class Etape(models.Model):
     machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
     quantite_ressource = models.ForeignKey(QuantiteRessource, on_delete=models.CASCADE)
     duree = models.IntegerField()
-    etape_suivante = models.ForeignKey(
-        "self", null=True, blank=True, on_delete=models.CASCADE
-    )
+    etape_suivante = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.nom} {self.machine} {self.quantite_ressource} {self.duree} {self.etape_suivante}"
@@ -83,12 +90,15 @@ class Produit(Objet):
     premiere_etape = models.ForeignKey(Etape, on_delete=models.PROTECT)
 
     def __str__(self):
-        return f"{self.premiere_etape}"
+        return f"{self.nom} {self.premiere_etape}"
 
 
 class Stock(models.Model):
-    objet = models.ForeignKey(Ressource, on_delete=models.PROTECT)
+    ressource = models.ForeignKey(Ressource, on_delete=models.PROTECT)
     nombre = models.IntegerField()
 
     def __str__(self):
-        return f"{self.objet} {self.nombre} "
+        return f"{self.ressource} {self.nombre}"
+
+    def costs(self):
+        return self.ressource.prix * self.nombre
