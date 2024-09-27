f06d9ce7d264b5a3d5b3e2b4a761bf0d1250e8d4
brl4010a <brl4010a@3a-g23-11d.univ-tlse3.fr>
2024-09-27 09:41:45 +0200

    add render to response

--- crayon/high_level/views.py
+++ crayon/high_level/views.py
@@ -1,23 +1,39 @@
 # Create your views here.
 from django.views.generic import DetailView
 from .models import Ville, Usine, Machine, Ressource, Stock
+from django.http import JsonResponse
 
 
 class VilleDetailView(DetailView):
     model = Ville
 
+    def render_to_response(self, *args, **kwargs):
+        return JsonResponse(self.object.json())
+
 
 class UsineDetailVieew(DetailView):
     model = Usine
+
+    def render_to_response(self, *args, **kwargs):
+        return JsonResponse(self.object.json())
 
 
 class MachineDetailView(DetailView):
     model = Machine
 
+    def render_to_response(self, *args, **kwargs):
+        return JsonResponse(self.object.json())
+
 
 class RessourceDetailView(DetailView):
     model = Ressource
 
+    def render_to_response(self, *args, **kwargs):
+        return JsonResponse(self.object.json())
+
 
 class StockDetailView(DetailView):
     model = Stock
+
+    def render_to_response(self, *args, **kwargs):
+        return JsonResponse(self.object.json())
