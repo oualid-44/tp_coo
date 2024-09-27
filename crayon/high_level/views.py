36f5b559e25cabdab5358279e60494ee41105a5f
oualid-44 <o.aberbour@outlook.com>
2024-09-27 09:16:31 +0200

    Add views

--- crayon/high_level/views.py
+++ crayon/high_level/views.py
@@ -1,1 +1,23 @@
 # Create your views here.
+from django.views.generic import DetailView
+from .models import Ville, Usine, Machine, Ressource, Stock
+
+
+class VilleDetailView(DetailView):
+    model = Ville
+
+
+class UsineDetailVieew(DetailView):
+    model = Usine
+
+
+class MachineDetailView(DetailView):
+    model = Machine
+
+
+class RessourceDetailView(DetailView):
+    model = Ressource
+
+
+class StockDetailView(DetailView):
+    model = Stock
