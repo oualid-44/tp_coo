fc5e683aa37c73c94e467ce75360fd8eebfc1793
brl4010a <brl4010a@u6-106-04d.univ-tlse3.fr>
2024-10-02 09:50:25 +0200

    Add a cpr request

--- crayon/low_level/low_level.cpp
+++ crayon/low_level/low_level.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 using namespace std;
+#include <cpr/cpr.h>
+
+#include <nlohmann/json.hpp>
 #include <string>
 
 class Ville {
@@ -11,6 +14,7 @@ class Ville {
  public:
   Ville(string name, int cp, int prix)
       : nom(name), code_postale(cp), prix_m2(prix) {};
+  ~Ville() {};
   friend ostream& operator<<(ostream& out, const Ville& v) {
     return out << "Nom: " << v.nom << " Code postale: " << v.code_postale
                << " Prix m2 :" << v.prix_m2 << endl;
@@ -21,6 +25,12 @@ auto main() -> int {
   Ville v("Toulouse", 31000, 100);
 
   cout << v;
+
+  cpr::Response r = cpr::Get(cpr::Url{"http://localhost:8000/ville/1"});
+  std::cout << r.url << std::endl;
+  std::cout << r.status_code << std::endl;
+  std::cout << r.header["content-type"] << std::endl;
+  std::cout << r.text << std::endl;
 
   return 0;
 }
